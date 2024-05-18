9648e24b394c69fcd92b766c8dc5f7ffc6d17191
七詩パンダ <danal99151@gmail.com>
2024-05-19 01:30:49 +0800

    make player function

--- player.js
+++ player.js
@@ -2,6 +2,16 @@
 var player = {};
 function initGame() {
     initMap();
+    player.HP = 100;
+    player.MAXHP = 100;
+    player.LV = 1;
+    player.exp = 0;
+    //expreq = floor(1<<0.3lv) + delta
+    player.atk = 10;
+    player.def = 0;
+    player.MAXMP = 100;
+    player.MP = 100;
+    player.speeed = 10;
     player.nowRoom = 0;
     player.getKey = 0;
     player.location = { x: 0.0, y: 0.0, z: map[player.nowRoom].field[0][0] }
@@ -34,8 +44,41 @@ function move(dx, dy) {
     if (nowplace.x === map[player.nowRoom].keyLocation.x && nowplace.y === map[player.nowRoom].leyLocation.y) {
         map[player.nowRoom].keyLocation = { x: -1, y: -1 };
         player.getKey++;
-        if (player.getKey == 3) {
+        if (player.getKey == 3 && leftmonster == 0) {
             boss = true
         }
     }
+}
+function regenHP(res) {
+    player.HP += res;
+    if (player.HP > player.MAXHP) player.HP = player.MAXHP;
+}
+function regenMP(res) {
+    player.MP += res;
+    if (player.MP > player.MAXMP) player.MP = player.MAXMP;
+}
+function getDamage(atk) {
+    player.HP -= (atk - Math.floor(player.def));
+    if (player.HP <= 0) {
+        return 1;
+
+    }
+    return 0;
+}
+function getXP(exp) {
+    player.exp += exp
+    if (player.exp >= 100) {
+        player.exp -= 100;
+        player.LV++;
+        player.MAXMP += 20;
+        player.MAXHP += 20;
+        player.atk += 1;
+        player.def += 0.2;
+        player.speeed += 0.5;
+        player.HP = player.MAXHP;
+        player.MP = player.MAXMP;
+        return 1;
+    }
+    return 0;
+
 }
